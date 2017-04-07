4252fa21abf72d9d6235dfaa5c8f3088a093553e
jdlawrence <jamil.lawrence@gmail.com>
2017-04-06 21:15:49 -0700

    Playing around with Dr. Racket

--- dummy.rkt
+++ dummy.rkt
@@ -1,2 +1,9 @@
 #lang sicp
-(inc 42)
+(define (square x) (* x x))
+(square 21)
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+(f 2)
+(= 7 7)
