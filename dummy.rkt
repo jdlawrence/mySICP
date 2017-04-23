dc89a211ed902426d4626d84f53359f067d2c17e
jdlawrence <jamil.lawrence@gmail.com>
2017-04-23 09:58:38 -0700

    Finished EX 1_1

--- dummy.rkt
+++ dummy.rkt
@@ -1,9 +1,2 @@
 #lang sicp
-(define (square x) (* x x))
-(square 21)
-(define (sum-of-squares x y)
-  (+ (square x) (square y)))
-(define (f a)
-  (sum-of-squares (+ a 1) (* a 2)))
-(f 2)
-(= 7 7)
+(- 9 1)
