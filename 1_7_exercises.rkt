e92c33065b4f67fc358658a263b8fcabaff2f577
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-03-07 20:21:05 -0800

    Finished 1_7

--- 1_7_exercises.rkt
+++ 1_7_exercises.rkt
@@ -3,7 +3,11 @@
   (/ (+ x y) 2))
 
 
-(average 2 3)
+;(average 2 3)
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
 
 (define (square x) (* x x))
 
@@ -14,14 +18,17 @@
   (< (abs (- (square guess) x)) 0.001))
 
 (define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+  (new-if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x)
                  x)))
 
 (define (sqrt x)
-  (sqrt-iter 1.0 x))
+  (sqrt-iter 1.00 x))
 
-(sqrt 4)
+;(sqrt 4)
 
-;comment
+;(new-if) causes an infinite because it is a procedure, and procedures
+; are evaluated with applicative-order evaluation. This means that all procedures
+; must be broken down (evaluated) until we get to PRIMITIVE OPERATORS (+, -, *, /, 1, 2, 3, etc)
+; Since sqrt-iter is recursive, it will never get down to primitive operators
