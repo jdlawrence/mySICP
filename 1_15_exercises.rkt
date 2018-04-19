964b4efa467fdd126c8f4a02b41b6952d98e0ce7
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-04-18 21:41:14 -0700

    Fixes up answer to 1.15

--- 1_15_exercises.rkt
+++ 1_15_exercises.rkt
@@ -10,7 +10,7 @@
 (sine 12.15)
 
 #|
-a) p is called 6 times because after the 6th call, p, the angle / 3 is
+a) p is called 5 times because after the 5th call, p, the "x", angle / 3 is
 0.005, which is less than 0.1 and sufficiently small enough.
 b) This is O(log(n)) because we're dividing a number of times
 |#
