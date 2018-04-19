210058adf8ac5ccbc552f18a3c2c0020b6233a86
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-04-18 21:38:03 -0700

    Finished 1.15

--- 1_15_exercises.rkt
+++ 1_15_exercises.rkt
@@ -10,13 +10,7 @@
 (sine 12.15)
 
 #|
-To make 11, there are 4 methods. They are:
-eleven 1s;
-one 5, six 1s;
-two 5s, one 1;
-one 10, one 1;
-Each of these methods requires calling (cc amount kind-of-coins) some fraction of n times.
-This is done for each of the k number of coins. Therefore the space used is O(n), where n is
-the amount we're making change for, and time is O(n ^ k), where n is the amount we're making
-change for, and k is the amount of types of coins.
+a) p is called 6 times because after the 6th call, p, the angle / 3 is
+0.005, which is less than 0.1 and sufficiently small enough.
+b) This is O(log(n)) because we're dividing a number of times
 |#
