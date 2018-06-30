0b454ba0f00f629039de41aeb12c9f1699bad996
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-06-29 21:57:54 -0700

    Finished 1.30

--- 1_30_exercises.rkt
+++ 1_30_exercises.rkt
@@ -1,15 +1,10 @@
 #lang sicp
-(define (pi-sum a b)
-  (if (> a b)
-      0
-      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
-
-;(* 8 (pi-sum 1 2000000))
-
 (define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a) (sum term (next a) next b))))
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a))))) 
+  (iter a 0))
 
 (define (inc a)
   (+ a 1))
@@ -20,15 +15,14 @@
 (define (sum-cubes a b)
   (sum cube a inc b))
 
-(sum-cubes 1 4)
+(sum-cubes 1 400000)
 
-(define (factorial n)
- (fact-iter 1 1 n))
-(define (fact-iter product counter max-count)
- (if (> counter max-count)
- product
- (fact-iter (* counter product)
- (+ counter 1)
- max-count)))
+(define (pi-sum a b) 
+  (define (pi-term x) 
+          (/ 1.0 (* x (+ x 2)))) 
+  (define (pi-next x) 
+          (+ x 4)) 
+  (sum pi-term a pi-next b))
 
-(factorial 4)
+(* 8 (pi-sum 1 2000000))
+
