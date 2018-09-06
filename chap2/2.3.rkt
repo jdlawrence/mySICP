067e47303397dc2a84c69574a882f34c0bc65e92
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-09-05 21:33:57 -0700

    2.3 Complete

--- chap2/2.3.rkt
+++ chap2/2.3.rkt
@@ -25,10 +25,73 @@
  (display (y-point p))
  (display ")"))
 
-; With 4 points
-; (in clockwise order from bottom left
-(define (make-rectangle p1 p2 p3 p4)
-  (cons (cons p1 p2) (cons p3 p4)))
+#|
+4 points in clockwise order:
+bottom-left, top-left, top-right, bottom-right |#
+(define (make-rectangle-points p1 p2 p3 p4)
+  (cons
+   (cons p1 p2)
+   (cons p3 p4)
+        ))
+
+#|
+2 segments. Assuming rectangle has vertical sides:
+One segment goes from bottom-left to top-left
+Second segment goes from top-right to bottom-left
+|#
+(define (make-rectangle-segments s1 s2)
+  (cons
+    (cons (start-segment s1) (end-segment s1))
+    (cons (start-segment s2) (end-segment s2))
+    ))
+
+(define (p1 rect)
+  (car (car rect)))
+
+(define (p2 rect)
+  (car (cdr rect)))
+
+(define (p3 rect)
+  (cdr (car rect)))
+
+(define (p4 rect)
+  (cdr (cdr rect)))
 
 (define (perimeter rect)
-  (+ (abs (- (x-point (car
+  (* 2 (+ (abs (- (x-point (p1 rect)) (x-point (p4 rect))))
+          (abs (- (y-point (p1 rect)) (y-point (p2 rect))))
+          )))
+
+(define (area rect)
+  (* (abs (- (x-point (p1 rect)) (x-point (p4 rect))))
+     (abs (- (y-point (p1 rect)) (y-point (p2 rect))))
+     ))
+
+; Testing points
+(define r1 (make-rectangle-points
+            (make-point 0 0)
+            (make-point 0 20)
+            (make-point 4 20)
+            (make-point 4 0)))
+
+(perimeter r1)
+(area r1)
+
+; Testing segments
+(define s1 (make-segment (make-point 0 0) (make-point 0 20)))
+(define s2 (make-segment (make-point 4 20) (make-point 4 0)))
+
+(define r2 (make-rectangle-segments s1 s2))
+
+(perimeter r2)
+(area r2)
+
+;Test with a rectangle that doesn't have vertical sides
+(define r3 (make-rectangle-points
+            (make-point 0 0)
+            (make-point -.707 .707)
+            (make-point 0 1.414)
+            (make-point .707 .707)))
+
+(perimeter r3)
+(area r3) 
