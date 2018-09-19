8bfcfdf9e961a0aa00d59aa2794f7d576bc31bd3
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-09-18 19:39:36 -0700

    Improves definition of addition in 2.6

--- chap2/2.6.rkt
+++ chap2/2.6.rkt
@@ -12,16 +12,27 @@
 
 (((add-1 zero) fEx) xVal) ; return 2
 
+(define one (add-1 zero))
+(define two (add-1 one))
+(define three (add-1 two))
+(define four (add-1 three))
+
 (add-1 zero)
 (lambda (f) (lambda (x) (f ((zero f) x) )))
 (lambda (f) (lambda (x) (f x) ))
-(f x)
+; Meaning essentially (f x)
 
 (add-1 one)
 (lambda (f) (lambda (x) (f ((one f) x) )))
 (lambda (f) (lambda (x) (f (f x)) ))
-(f (f x))
+; Meaning essentially (f (f x))
 
+(define (add m n)
+  (lambda (f)
+    (lambda (x)
+      ((m f) ((n f) x)))))
+
+(((add three four) fEx) xVal) ; Outputs 14 because our f adds two everytime
 #|
 one is (f x)
 two is (f (f x))
