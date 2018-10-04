970c3160ccf955e024e7fd4eec5c931cbfdedf2f
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-10-03 20:58:53 -0700

    2.9 Complete

--- chap2/2.9.rkt
+++ chap2/2.9.rkt
@@ -20,8 +20,14 @@
     (make-interval (min p1 p2 p3 p4)
                    (max p1 p2 p3 p4))))
 
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+
 (define i1 (make-interval 3 7))
-(define i2 (make-interval 0 2))
+(define i2 (make-interval -1 1))
 (define i3 (make-interval -9 -5))
 (define i4 (make-interval 11 13))
 
@@ -32,6 +38,20 @@
 #| Testing |#
 (width (add-interval i1 i2))
 (width (add-interval i3 i4))
+#| Above widths are the same |#
+
+(width (sub-interval i1 i2))
+(width (sub-interval i3 i4))
+#| Above widths are the same |#
 
 (width (mul-interval i1 i2))
 (width (mul-interval i3 i4))
+#| Above widths are different |#
+
+(width (div-interval i1 i2))
+(width (div-interval i3 i4))
+#| Above widths are different |#
+
+#| Conclusion: The width of the sum or difference of two interval depends only
+on the input widths, not the absolute position of the intervals. This is not
+true for multiplication and division. |#
