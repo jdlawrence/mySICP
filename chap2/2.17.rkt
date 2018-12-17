5dbd90d23b3600170fa7653812b30e65f936e947
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-12-16 20:52:51 -0800

    2.17 improved

--- chap2/2.17.rkt
+++ chap2/2.17.rkt
@@ -7,9 +7,29 @@
         (sub (cdr subList) (car subList))))
   (sub aList (car aList)))
 
+#| Alternate version doesn't need a carry along variable
+and also won't fail for an empty list |#
+(define (last-pair-alt aList)
+  (define (sub nonEmptyList)
+    (if (null? (cdr nonEmptyList))
+      (list (car nonEmptyList))
+      (last-pair-alt (cdr nonEmptyList))))
+  (if (null? aList)
+      aList
+      (sub aList)))
+
+#| Testing |#
 (define jamil (list 1 2 3 4))
 
 (last-pair jamil)
 
 (last-pair (list 23 72 149 34))
+
+(last-pair-alt (list 23 72 149 34))
+
+(define emptyList (cdr (list 1)))
+
+(last-pair-alt emptyList)
+
+
 
