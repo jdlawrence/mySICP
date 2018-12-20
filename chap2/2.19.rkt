66e993484f7095294a182e2b602654517675db92
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-12-20 12:20:26 -0800

    Adds "no-more?" function

--- chap2/2.19.rkt
+++ chap2/2.19.rkt
@@ -7,6 +7,11 @@
 (define (except-first-denomination aList)
   (cdr aList))
 
+(define (no-more? aList)
+  (null? aList))
+
 #| Testing |#
-(first-denomination us-coins)
-(except-first-denomination us-coins)
+(first-denomination us-coins) ; 50
+(except-first-denomination us-coins) ; (25 10 5 1)
+(no-more? us-coins) ; false
+(no-more? (list)) ; true
