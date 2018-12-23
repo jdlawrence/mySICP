098c010586a477b625751dcc6bc3915827e6c9a4
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-12-22 18:17:25 -0600

    2.19 mostly complete

--- chap2/2.19.rkt
+++ chap2/2.19.rkt
@@ -1,5 +1,27 @@
 #lang racket
-(define us-coins (list 50 25 10 5 1))
+#| Old version from 1.2.2 
+(define (count-change amount)
+  (cc amount 5))
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+|#
+
+#| 
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+|#
+
+(define us-coins (list 25 50 10 5 1))
 
 (define (first-denomination aList)
   (car aList))
@@ -10,8 +32,24 @@
 (define (no-more? aList)
   (null? aList))
 
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+#| ***** Answer *****
+The order of the coins does not matter because eventually all coins will be used
+|#
+
+
 #| Testing |#
 (first-denomination us-coins) ; 50
 (except-first-denomination us-coins) ; (25 10 5 1)
 (no-more? us-coins) ; false
 (no-more? (list)) ; true
+
+(cc 100 us-coins)
