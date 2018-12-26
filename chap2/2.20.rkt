e62545ab3a994bbd3f9c4e2ad9445f828018821a
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-12-26 15:12:57 -0600

    Finished up 2.20 an alternate approach

--- chap2/2.20.rkt
+++ chap2/2.20.rkt
@@ -1,5 +1,4 @@
 #lang racket
-
 (define (append list1 list2)
   (if (null? list1)
       list2
@@ -24,9 +23,29 @@
       (filter-odd args (list))
       (filter-even args (list))))
 
+#| Alternative version that doesn't need that "Append" helper |#
+(define (same-parity2 . args)
+  (define (sub aList parity)
+       (cond ((null? aList) '()) ; Note that you must use '() if using #lang racket and not #lang sicp
+             ((parity (car aList)) (cons (car aList) (sub (cdr aList) parity)))
+          (else (sub (cdr aList) parity))))
+  (if (even? (car args))
+      (sub args even?)
+      (sub args odd?)))
+
+(define (map proc items)
+  (if (null? items)
+      '()
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (mapDivideBy3 items)
+  (map (lambda (x) (/ x 3)) items))
 
 #| Testing |#
-(filter-odd (list 1 2 3 4 5 6) (list))
-(filter-even (list 1 2 3 4 5 6) (list))
-(same-parity 1 2 3 4 5 6 7)
-(same-parity 2 3 4 5 6 7)
+(filter-odd (list 1 2 3 4 5 6) (list)) ; '(1 3 5)
+(filter-even (list 1 2 3 4 5 6) (list)) ; '(2 4 6)
+(same-parity 1 2 3 4 5 6 7) ; '(1 3 5 7)
+(same-parity 2 3 4 5 6 7) ; '(2 4 6)
+(same-parity2 2 3 4 5 6 7 8 9 10 11 12) ; '(2 4 6 8 10 12)
+(mapDivideBy3 (list 3 6 9 12 15 18)) ; '(1 2 3 4 5 6)
--- dummy.rkt
+++ dummy.rkt
@@ -1,3 +1,9 @@
-#lang sicp
-(- 9 1)
-(inc 42)
+#lang racket
+(define (test val parity)
+  (if (parity val)
+      true
+      false))
+
+(test 2 odd?)
+
+(append (list 1 2 ) (list 3 4))
