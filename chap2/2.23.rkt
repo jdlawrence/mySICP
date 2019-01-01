79b42b59f3e7c75a5c8891d480521212a0906b97
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-01-01 14:44:36 -0800

    2.23 improved slightly

--- chap2/2.23.rkt
+++ chap2/2.23.rkt
@@ -6,7 +6,18 @@
   (if (null? items)
       #t
       (cons (proc (car items)) (my-for-each proc (cdr items)))))
+
+(define (my-for-each-2 proc items)
+  (if (null? items)
+      #t
+      (let ()
+        (proc (car items))
+        (my-for-each-2 proc (cdr items)))))
+
+    
        
 (my-for-each (lambda (x) (newline) (display x))
           (list 57 321 88))
 
+(my-for-each-2 (lambda (x) (newline) (display x))
+               (list 1 2 3))
