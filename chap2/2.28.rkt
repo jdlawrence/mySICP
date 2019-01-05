9b70d1c42f26cf991475d2d7df8ddd1cf4432dd4
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-01-05 13:29:19 -0800

    2.28 in progress

---
+++ chap2/2.28.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define x (list (list 1 2) (list 3 4)))
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+(define (fringe
