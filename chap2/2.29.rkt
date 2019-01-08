5917bd70c4938eb2167677f6b30e152a3b228b06
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-01-07 21:18:09 -0800

    2.29 in progress

---
+++ chap2/2.29.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (cdr mobile))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (cdr branch))
+
+(define (total-weight mobile)
+
