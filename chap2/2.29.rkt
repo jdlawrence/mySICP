c51e6c3a47e7ab29b5833a072b0f0a6664ff79d2
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-01-10 08:45:35 -0800

    2.29 complete

--- chap2/2.29.rkt
+++ chap2/2.29.rkt
@@ -25,31 +25,43 @@
   (+ (weight-sub (left-branch mobile))
      (weight-sub (right-branch mobile))))
 
-(define (balanced? mobile)
-  (define (torque branch)
+(define (torque branch)
     (if (pair? (branch-structure branch))
         (* (branch-length branch) (total-weight (branch-structure branch)))
         (* (branch-length branch) (branch-structure branch))))
-  (= (torque (left-branch mobile)) (torque (right-branch mobile))))
+
+(define (balanced? mobile)
+  (and (= (torque (left-branch mobile)) (torque (right-branch mobile)))
+       (if (pair? (branch-structure (left-branch mobile)))
+           (balanced? (branch-structure (left-branch mobile)))
+           #t)
+       (if (pair? (branch-structure (right-branch mobile)))
+           (balanced? (branch-structure (right-branch mobile)))
+           #t)
+       ))
 
 (define branch1 (make-branch 3 8))
 
-(define branch2 (make-branch 4 6))
+;(define branch2 (make-branch 4 6))
 
-(define branch3 (make-branch 6 (make-mobile (make-branch 2 11) (make-branch 7 13))))
+;(define branch3 (make-branch 6 (make-mobile (make-branch 2 11) (make-branch 7 13))))
 
-(define mobile1 (make-mobile branch1 branch2))
+(define branch4 (make-branch 2 (make-mobile (make-branch 2 6) (make-branch 2 6))))
 
-(define mobile2 (make-mobile branch1 branch3))
+;(define mobile1 (make-mobile branch1 branch2))
+
+(define mobile2 (make-mobile branch1 branch4))
 
 
 ;mobile1
 ;(branch-length (make-branch 3 5))
 ;(pair? (branch-structure (make-branch 3 5)))
 ;(pair? (branch-structure (make-branch 3 (make-mobile (make-branch 2 11) (make-branch 7 13)))))
-(right-branch mobile1)
-(total-weight mobile1)
+;(right-branch mobile1)
+;(total-weight mobile1)
 
-(total-weight mobile2)
-(balanced? mobile1)
+;(total-weight mobile2)
+;(torque branch3)
+(torque branch4)
+(balanced? mobile2)
 ;mobile2
