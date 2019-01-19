b6217100455e231343b082a6859d0a69ddd67084
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-01-18 21:31:04 -0800

    2.32 complete but need to understand thorougly

--- chap2/2.33.rkt
+++ chap2/2.33.rkt
@@ -1,9 +1,24 @@
 #lang racket
 (define nil '())
 
+(define (j item) item)
+
 (define (subsets s)
   (if (null? s)
       (list nil)
       (let ((rest (subsets (cdr s))))
-        (append rest (map <??> rest)))))
+        (append rest (map (lambda (items) (cons (car s) items)) rest)))))
 
+(subsets (list 1 2 3))
+
+(define (f x)
+  ((lambda (a) (* a a))
+   x))
+
+;(f 1.3)
+
+(define (map-square items)
+  (map f items))
+
+;(map-square (list 1 2 3 45))
+  
