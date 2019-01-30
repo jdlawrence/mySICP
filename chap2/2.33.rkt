167c8239b33f59141bbee5b030f05e74a58aef77
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-01-29 21:01:08 -0800

    Finished map from 2.33

--- chap2/2.33.rkt
+++ chap2/2.33.rkt
@@ -5,4 +5,28 @@
        (op (car sequence)
            (accumulate op initial (cdr sequence)))))
 
+
+(define (fib n)
+  (define (sub curr prior count)
+    (cond ((= n 0) 0)
+          ((= n 1) 1)
+          ((= n count) curr)
+          (else (sub (+ curr prior) curr (+ count 1)))))
+  (sub 1 0 2))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (map-my-version p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(map-my-version (lambda (x) (* x x)) (list 1 2 3 4 5))
+
+#| Testing |#
 (accumulate + 0 (list 1 2 3 4 5))
+(map fib (enumerate-interval 0 6))
+(fib 1)          
+(fib 2)          
+(fib 3)          
