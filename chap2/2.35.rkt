5cdccdcf2dfbd101470bf9253ffa03a4d20c3b17
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-02-07 21:21:37 -0800

    2.35 complete

--- chap2/2.35.rkt
+++ chap2/2.35.rkt
@@ -4,5 +4,25 @@
       initial
       (op (car items) (accumulate op initial (cdr items)))))
 
-(define (count-leaves t)
-  (accumulate + ⟨??⟩ (map ⟨??⟩ ⟨??⟩)))
+#| My answer |#
+(define (count-leaves-alt t)
+  (define (counter item)
+    (cond ((null? item) 0)
+          ((not (pair? item)) 1)
+          (else (count-leaves-alt item))))
+  (accumulate + 0 (map counter t)))
+
+#| The earlier answer from 2.2.2 |#
+(define (count-leaves x)
+   (cond ((null? x) 0)
+         ((not (pair? x)) 1)
+         (else (+ (count-leaves (car x))
+                  (count-leaves (cdr x))))))
+
+#| Testing |#
+(define ex1 (list (list 1 2) 3 4))
+(define ex2 (list 3 4 5 (list 6 (list 7 8) 9)))
+(count-leaves ex1)
+(count-leaves ex2)
+(count-leaves-alt ex1)
+(count-leaves-alt ex2)
