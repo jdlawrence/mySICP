3922c886298b02d0ce96ff56c07ef64d236ef28d
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-02-21 12:17:32 -0800

    2.41 in progress

---
+++ chap2/2.41.rkt
@@ -0,0 +1,54 @@
+#lang racket
+(define (square n) (* n n))
+(define nil '())
+(define (accumulate op initial items)
+  (if (null? items)
+      initial
+      (op (car items) (accumulate op initial (cdr items)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+#| Answers
+|#
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+                   (map (lambda (j) (list i j))
+                        (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (unique-triplet n)
+  (flatmap (lambda (i)
+                   (map (lambda (j)
+                          (map (lambda (k) (list i j k))
+                               (enumerate-interval 1 (- j 1))))
+                        (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+#|
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (flatmap
+                           (lambda (i)
+                             (map (lambda (j) (list i j))
+                                  (enumerate-interval 1 (- i 1))))
+                           (enumerate-interval 1 n)))))
+
+(define (prime-sum-pairs-simple n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+|#
+
+#| Testing |#
+(unique-pairs 6)
+(unique-triplet 4)
+;(prime-sum-pairs 6)
+;(prime-sum-pairs-simple 6)
