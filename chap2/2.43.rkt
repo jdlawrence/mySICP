1edc7fc079089709756297ebda8efdf86ba5fa79
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-04-21 21:47:49 -0700

    2.43 in progress

---
+++ chap2/2.43.rkt
@@ -0,0 +1,28 @@
+#lang racket
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position
+                    new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+#| Louis's code:
+|#
+
+(flatmap
+ (lambda (new-row)
+   (map (lambda (rest-of-queens)
+          (adjoin-position new-row k rest-of-queens))
+        (queen-cols (- k 1))))
+ (enumerate-interval 1 board-size))
+#| Answer:
+
+By flipping the qu
