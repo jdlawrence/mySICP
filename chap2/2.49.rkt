723698c286be8884f5f9229cfc974cd43500a537
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-05-15 21:07:20 -0700

    2.49 in progress

---
+++ chap2/2.49.rkt
@@ -0,0 +1,67 @@
+#lang racket
+(#%require sicp-pict)
+#|
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v) (edge1-frame frame))
+               (scale-vect (ycor-vect v) (edge2-frame frame))))))
+|#
+
+#| From 2.46 |#
+(define (make-vect xcor ycor)
+  (cons xcor ycor))
+
+(define (xcor-vect vect)
+  (car vect))
+
+(define (ycor-vect vect)
+  (cdr vect))
+
+(define (add-vect vect1 vect2)
+  (make-vect (+ (xcor-vect vect1) (xcor-vect vect2))
+             (+ (ycor-vect vect1) (ycor-vect vect2))))
+
+(define (sub-vect vect1 vect2)
+  (make-vect (- (xcor-vect vect1) (xcor-vect vect2))
+             (- (ycor-vect vect1) (ycor-vect vect2))))
+
+(define (scale-vect scalar vect)
+  (make-vect (* scalar (xcor-vect vect))
+             (* scalar (ycor-vect vect))))   
+
+
+#| Answers |#
+(define (make-segment origin-start start-end)
+  (cons origin-start start-end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+#| Testing |#
+(define ex1 (make-segment (make-vect 0 4) (make-vect 4 9)))
+(start-segment ex1) ;(0 4)
+(end-segment ex1 ) ; (4 9)
+
+(for-each (lambda (x)
+            (newline)
+            (display x))
+          (list 57 321 88))
+
+#|
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame)
+         (start-segment segment))
+        ((frame-coord-map frame)
+         (end-segment segment))))
+     segment-list)))
+
+|#
