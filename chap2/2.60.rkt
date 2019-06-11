a83c0f0cbde3d2841a39ec6860776bbfab74165a
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-06-10 21:29:57 -0700

    2.60 in progress

---
+++ chap2/2.60.rkt
@@ -0,0 +1,12 @@
+#lang racket
+#| Answer |#
+#| element-of-set doesn't change at all |#
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define l1 (list 2 3 2 1 3 2 2))
+
+(element-of-set? 3 l1)
+(element-of-set? 13 l1)
