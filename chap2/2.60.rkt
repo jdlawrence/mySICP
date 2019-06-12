ca89f71a3d408fcad290017a12bff811464a60fb
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-06-11 22:07:35 -0700

    2.60 still in progress

--- chap2/2.60.rkt
+++ chap2/2.60.rkt
@@ -6,6 +6,13 @@
         ((equal? x (car set)) true)
         (else (element-of-set? x (cdr set)))))
 
+
+#| Adjoin-set also doens't change at all |#
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
 (define l1 (list 2 3 2 1 3 2 2))
 
 (element-of-set? 3 l1)
