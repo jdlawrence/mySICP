b8c35c2389388c755cda300ccdeb7e7e3c632de7
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-06-16 17:18:49 -0700

    2.62 error fixed

--- chap2/2.62.rkt
+++ chap2/2.62.rkt
@@ -7,13 +7,15 @@ Time complexity is now O(n)
 (define (union-set s1 s2)
   (cond ((null? s1) s2)
         ((null? s2) s1)
-        ((= (car s1) (car s2)) (union-set (cdr s1) s2))
+        ((= (car s1) (car s2)) (cons (car s1) (union-set (cdr s1) (cdr s2))))
         ((< (car s1) (car s2)) (cons (car s1) (union-set (cdr s1) s2)))
+        ((< (car s2) (car s1)) (cons (car s2) (union-set s1 (cdr s2))))
         (else (union-set (cdr s1) s2))))
 
 #| Testing |# 
 (define l1 (list 1 2 3))
 (define l2 (list 3 4 5 6))
-(define l3 (list 1))
+(define l3 (list 1 3 5 7))
+(define l4 (list 2 4 6 8))
 
-(union-set l1 l2)
+(union-set l3 l4)
