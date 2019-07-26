ae8bfebaf5da4c1b0d86f9c5d13031c307f992ee
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-07-26 15:14:25 -0700

    2.66 complete with testing

--- chap2/2.66.rkt
+++ chap2/2.66.rkt
@@ -17,8 +17,6 @@
 
 (define j (make-record 1 "jamil"))
 
-(key j)
-(value j)
 
 #|
 (define (lookup-unordered given-key set-of-records)
@@ -29,8 +27,44 @@
 |#
 
 
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result
+               (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result
+                   (partial-tree
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts
+                     (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+
+
+
 (define (lookup given-key set-of-records)
   (cond ((null? set-of-records) false)
         ((equal? given-key (key (entry set-of-records))) (entry set-of-records))
         ((< given-key (key (entry set-of-records))) (lookup given-key (left-branch set-of-records)))
         (else (lookup given-key (right-branch set-of-records)))))
+
+#| Testing |#
+(define l1 (list (make-record 1 "jamil")
+      (make-record 2 "akeem")
+      (make-record 3 "kyle")
+      (make-record 4 "robin")))
+
+(list->tree l1)
+(lookup 5 (list->tree l1))
