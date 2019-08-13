6eecb4472aec9efcf169a70400809a90cf63a0c5
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-08-12 22:30:38 -0700

    2.72 in progress

---
+++ chap2/2.72.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+#| Answer:
+I believe the answer is somewhere in the neighborhood of O(n^2). This is is because at each leaf you have to search the leaf list, and you have visit all nodes.
+|#
