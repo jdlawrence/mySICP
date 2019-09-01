43330e1e30295edfd655933b74a50e798db69868
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-09-01 16:14:05 -0700

    Part A complete

--- chap2/2.74.rkt
+++ chap2/2.74.rkt
@@ -1,23 +1,46 @@
 #lang racket
 
-(define (deriv exp var)
-  (cond ((number? exp) 0)
-        ((variable? exp) (if (same-variable? exp var) 1 0))
-        ((get (operator exp) 'deriv) (operands exp) var)
-        (else ((get 'deriv (operator exp))
-               (operands exp) var))))
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
-
-(put '(deriv) '+ (lambda (exp var) (make-sum
-                           (deriv (addend exp) var) (deriv (augend exp) var))))
-(put '(deriv) '* (lambda (exp var) (make-sum
-                           (make-product
-                            (multiplier exp) (deriv (multiplicand exp) var))
-                           (make-product
-                            (deriv (multiplier exp) var)  (multiplicand exp))))
-(put 'exponentiation (deriv) (lambda (exp var)  (make-product
-                                                 (make-product (exponent exp) (make-exponent (base exp) (make-sum (exponent exp) -1))))
-                                                 (deriv (base exp) var)))
-#| Answer:
+#| Earlier in the text, we were given this function:
 |#
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+#|
+If we wanted, we could make a more specific version of this.
+Let's call it "make-standard-record"
+|#
+
+(define (make-standard-record division record)
+  (cons division record))
+
+(define (get-division standard-record)
+  (car standard-record))
+
+(define (get-content standard-record)
+  (cdr standard-record))
+
+
+
+ 
+#| Part A Answer:
+Standard records must include division so that we can look up the type of "get-record" and
+"get-salary" functions to use.
+
+The "get" function takes two parameters, a function name, like "get-record" and a division, so
+that we can know which type of function to retrieve.
+
+Once we retrieve the function, we pass it the employee's name, and the contents of file (we use
+"get-content" to remove the division and get just the contents)
+
+If the record doesn't exist, return null. If it does, make it a standard record by attaching
+division in front of it.
+
+|#
+(define (get-record emp-name a-standard-file)
+  (let (
+        (record ((get 'get-record (get-division a-standard-file)) emp-name (get-content a-stardard-record)))
+    )
+    (if (null? record)
+        '()
+        (make-standard-record (get-division a-standard-record) record))))
+
