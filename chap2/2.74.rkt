e69597d78292515799c09c049a6b1697b370b18c
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-09-01 17:06:13 -0700

    2.74 complete

--- chap2/2.74.rkt
+++ chap2/2.74.rkt
@@ -36,11 +36,47 @@ If the record doesn't exist, return null. If it does, make it a standard record 
 division in front of it.
 
 |#
-(define (get-record emp-name a-standard-file)
+(define (get-record emp-name a-standard-record)
   (let (
-        (record ((get 'get-record (get-division a-standard-file)) emp-name (get-content a-stardard-record)))
-    )
+        (record ((get 'get-record (get-division a-standard-record)) emp-name (get-content a-stardard-record)))
+        )
     (if (null? record)
         '()
         (make-standard-record (get-division a-standard-record) record))))
 
+#| Part B Answer:
+To work for any division, this employee's record must tell which division they're from.
+
+Again, we use the division type to determine which "get-salary" function to retrieve. Once we have that,
+we pass the content of the record to the function.
+
+If the record doesn't exist, return null, else add the division to the salary.
+|#
+(define (get-salary a-standard-record)
+  (let (
+        (record ((get 'get-salary (get-division a-standard-record)) (get-content a-stardard-record)))
+        )
+    (if (null? record)
+        '()
+        (make-standard-record (get-division a-standard-record) record))))
+
+#| Part C Answer:
+- Loop all over division records, calling "get-record" for each one until the name matches
+or we reach the end of list. To check for name matching, we need a "get-name" function
+that takes in a standard-record and the division and returns the name
+
+|#
+
+(define (find-employee-record emp-name list-of-division-records)
+  (cond ((null? list-of-division-records) '())
+        (else
+         (let ((record (get-record emp-name (car list-of-division-records))))
+              (if (null? record)
+                  (find-employee-record emp-name (cdr list-of-division-records))
+                  record)))))
+#| Part D Answer:
+When Insatiable takes over, a package must be created that tag each company's "get-record",
+"get-salary" etc functions for general use using "put". Once this is done, we use the
+"get-division" function to determine which division's function to use, and they can implement
+the functions and underlying data structures however they like.
+|#
--- dummy.rkt
+++ dummy.rkt
@@ -8,3 +8,22 @@
 (cddddr ex)
 
 (cddr ex2)
+
+(define (add2 x)
+  (let ((a 5)
+    (b 6))
+    (+ a b x)))
+
+(define (even-or-odd n)
+  (if (= 0 (modulo n 2))
+      'even
+      'odd))
+
+
+(even-or-odd 7)
+(even-or-odd 6)
+(add2 3)
+
+(equal? 'jamil 'jamil)
+(equal? 'jamil 'kyle)
+(null? '())
