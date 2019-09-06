4b56a24cb384a2b2ae04fce08766b22d2868d0ec
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-09-05 22:04:13 -0700

    2.76 continued

--- chap2/2.76.rkt
+++ chap2/2.76.rkt
@@ -19,7 +19,27 @@
           ((eq? op 'angle) angle)
           (else (error "Unknown op: MAKE-FROM-MAG-ANGLE" op))))
   dispatch)
-
+(define (real-part z)
+  (cond ((rectangular? z)
+         (real-part-rectangular (contents z)))
+        ((polar? z)
+         (real-part-polar (contents z)))
+        (else (error "Unknown type: REAL-PART" z))))
 #| Answer:
+In the generic operations with explicity detail strategy, you must create a whole new
+series of functions per type that is added. For example, if we already have real-part-rectangular
+and real-part-polar, and we wanted to add a new type, say "crazy", we have to create
+real-part-crazy and also go and add it to the function that selects the appropriate real-part
+function, ie:
+(define (real-part z)
+  (cond ((rectangular? z)
+         (real-part-rectangular (contents z)))
+        ((polar? z)
+         (real-part-polar (contents z)))
+        ((crazy? z)
+         (real-part-crazy (contents z)))
+        (else (error "Unknown type: REAL-PART" z))))
+This is not convenient in either adding operations or types.
+
 
 |#
