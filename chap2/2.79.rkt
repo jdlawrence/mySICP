a581efcb6a95370011287788cfec9131fd346334
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-09-19 17:59:41 -0700

    2.79 complete

--- chap2/2.79.rkt
+++ chap2/2.79.rkt
@@ -3,5 +3,26 @@
 ;  (cond (
 (define (equ? x y) (apply-generic 'equ? x y))
 
-(eq? 'j 'j)
-(eq? 'j 'k)
+#| Inside the scheme-number package |#
+(define (equ?-scheme x y)
+  (= x y))
+#| Install in numbers generic package |#
+(put 'equ? '(rational-number rational-number)
+  (lambda (x y) (equ?-scheme x y)))
+
+
+#| Inside the scheme-number package |#
+(define (equ?-rational x y)
+  (and (= (numer x) (numer y))
+       (= (denom x) (denom y))))
+#| Install in numbers generic package |#
+(put 'equ? '(rational-number rational-number)
+  (lambda (x y) (equ?-rational x y)))
+
+#| Inside the complex-number package |#
+(define (equ?-complex x y)
+  (and (= (real-part x) (real-part y))
+       (= (imag-part x) (imag-part y))))
+#| Install in numbers generic package |#
+(put 'equ? '(complex-number complex-number)
+  (lambda (x y) (equ?-complex x y)))
