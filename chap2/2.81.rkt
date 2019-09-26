7d283cbd0b5ad97cc3c926c7718933a62f71891a
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-09-25 22:11:23 -0700

    2.81 in progress

---
+++ chap2/2.81.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else (error "No method for these types"
+                                     (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+
+#| Answer:
+a) If we add coercing a type to itself, and call exp with two complex numbers (for which there
+is no procedure, we will get an infinite loop with the current apply-generic. This is because
+we won't find the procedure, and we will always have have a successful coercision, which
+recursively calls apply-generic again.
+
+b)
