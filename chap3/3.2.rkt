5f621de0077da18a81469e96847f6491f16c6d94
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-10-27 11:32:43 -0700

    3.2 in progress

---
+++ chap3/3.2.rkt
@@ -0,0 +1,17 @@
+#lang racket
+(define (make-monitored f)
+  (let ((count 0)
+  (set! count 0)
+  count)
+#|
+  (lambda (arg)
+    (set! count (
+|#
+#| Testing |#
+
+#| Answer |#
+
+
+   
+               
+                     
--- dummy.rkt
+++ dummy.rkt
@@ -1,42 +1,61 @@
 #lang racket
-(define ex (list 1 2 3 4 5 6 7 8))
-(define ex2 (list '+ 1 2))
+#|
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else (error "No method for these types"
+                                     (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
 
-(cadr ex)
-(cddr ex)
-(cdddr ex)
-(cddddr ex)
+|#
+#| Answer:
+a) If we add coercing a type to itself, and call exp with two complex numbers (for which there
+is no procedure, we will get an infinite loop with the current apply-generic. This is because
+we won't find the procedure, and we will always have have a successful coercision, which
+recursively calls apply-generic again.
 
-(cddr ex2)
+b) Louis Reasoner is wrong that something needs to change about apply-generic. If we don't change apply-
+generic currently, we will just get "No method for these types", which is true.
 
-(define (add2 x)
-  (let ((a 5)
-    (b 6))
-    (+ a b x)))
+c) No alteration necessary
+Altered apply-generic below:
+|#
 
-(define (even-or-odd n)
-  (if (= 0 (modulo n 2))
-      'even
-      'odd))
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond ((eq? type1 type2) (error
+                    (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else (error "No method for these types"
+                                     (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
 
 
-(even-or-odd 7)
-(even-or-odd 6)
-(add2 3)
-
-(equal? 'jamil 'jamil)
-(equal? 'jamil 'kyle)
-(null? '())
-
-(define (cons x y)
-  (define (dispatch m)
-    (cond ((= m 0) x)
-          ((= m 1) y)
-          (else (error "Argument not 0 or 1: CONS" m))))
-  dispatch)
-
-(define (car z) (z 0))
-(define (cdr z) (z 1))
-
-(car (cons 3 4))
-(cdr (cons 3 4))
+(eq? 'a 'a)
