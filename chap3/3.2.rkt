6e6a1fba3d87107a42cc893ef7dd027b616e9561
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-10-27 15:44:20 -0700

    Move 3.2

--- chap3/3.2.rkt
+++ chap3/3.2.rkt
@@ -1,17 +1,34 @@
 #lang racket
-(define (make-monitored f)
-  (let ((count 0)
-  (set! count 0)
-  count)
-#|
-  (lambda (arg)
-    (set! count (
-|#
-#| Testing |#
-
 #| Answer |#
+(define (make-monitored f)
+  ;(let ((count 0))
+    (define count 0)
+    (lambda (arg)
+      (cond ((eq? arg 'how-many-calls) count)
+            ((eq? arg 'reset-count) (set! count 0) count)
+            (else 
+             (set! count (+ count 1))
+             (f arg)
+             ))))
+#| Testing |#
+(define (double n)
+  (* 2 n))
+ 
+  
 
-
+(define m-double (make-monitored double))
+(m-double 4)
+(m-double 5)
+(m-double 'how-many-calls)
+(m-double 5)
+(m-double 6)
+(m-double 'how-many-calls)
+(m-double 'reset-count)
+(m-double 'how-many-calls)
+(m-double 7)
+(m-double 8)
+(m-double 9)
+(m-double 'how-many-calls)
    
                
                      
