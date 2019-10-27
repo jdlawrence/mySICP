374059a11ebf9e84bcc9598f2f46e7b8cfe1a524
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-10-27 12:56:28 -0700

    3.2 clean up

--- chap2/3.2.rkt
+++ chap2/3.2.rkt
@@ -4,11 +4,11 @@
   (let ((count 0))
     (lambda (arg)
       (cond ((eq? arg 'how-many-calls) count)
-            ((eq? arg 'reset-count) (set! count 0))
-            (else (begin
-                    (set! count (+ count 1))
-                    (f arg)
-                    ))))))
+            ((eq? arg 'reset-count) (set! count 0) count)
+            (else 
+             (set! count (+ count 1))
+             (f arg)
+             )))))
 #| Testing |#
 (define (double n)
   (* 2 n))
