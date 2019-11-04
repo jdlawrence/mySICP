b9ca9bb7ae33436686933a0262c9d3ebfabee241
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-11-03 20:39:31 -0800

    Clean up 3.8 a bit

--- chap3/3.8.rkt
+++ chap3/3.8.rkt
@@ -1,30 +1,15 @@
 #lang racket
-(define (make-monitored f)
-  ;(let ((count 0))
-    (define count 0)
-    (lambda (arg)
-      (cond ((eq? arg 'how-many-calls) count)
-            ((eq? arg 'reset-count) (set! count 0) count)
-            (else 
-             (set! count (+ count 1))
-             (f arg)
-             ))))
-
 #| Answer |#
 (define (left-right-diff)
   (define count -1)
   (lambda (arg)
     (set! count (+ count 1))
     (if (= count 0)
-        (if (= arg 1)
-            1
-            0)
-        0)
-  ))
+        arg
+        0)))
   
 
 #| Testing |#
 (define f (left-right-diff))
-;(f 0)
-(f 1)
-(f 0)
+;(+ (f 0) (f 1)) returns 0
+(+ (f 1) (f 0)) ;returns 1 (When it's the only function executed)
