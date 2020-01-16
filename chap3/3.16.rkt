beb9f3d086a61c356cfba7710bc80db45de8263c
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-01-15 21:05:32 -0800

    3.16 finished

--- chap3/3.16.rkt
+++ chap3/3.16.rkt
@@ -1,4 +1,4 @@
-#lang racket
+#lang sicp
 (define (count-pairs x)
   (if (not (pair? x))
       0
@@ -15,11 +15,15 @@ where the car and cdr point to the same pair
 (define 1-pair (cons 'a 'b))
 (define intermediate-2 (cons 1-pair 1-pair))
 (define 3-pair-three (cons 'a (cons 'b (cons 'c '()))))
-(define 4-pair-three (cons 'a (cons 1-pair 1-pair)))
-(define 7-pair-three (cons intermediate intermediate))
+(define 4-pair-three (cons 'a intermediate-2))
+(define 7-pair-three (cons intermediate-2 intermediate-2))
                                                    
 (count-pairs 1-pair)
-(count-pairs intermediate)
+(count-pairs intermediate-2)
 (count-pairs 3-pair-three)
 (count-pairs 4-pair-three)
 (count-pairs 7-pair-three)
+
+(define infinite (cons 'a (cons 'b (cons 'c '()))))
+(set-cdr! (cddr infinite) infinite)
+(count-pairs infinite) ;Never returns 
