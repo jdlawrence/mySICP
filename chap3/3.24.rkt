66bb3791facf9f9e88cec846b2c8f1e8b98254dc
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-03-29 20:46:22 -0700

    3.24 in progress

---
+++ chap3/3.24.rkt
@@ -0,0 +1,38 @@
+#lang sicp
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record (cdr record) false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable
+             (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record
+                   (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1 (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+(define t (make-table))
+(define get (t 'lookup-proc))
+(define put (t 'insert-proc!))
+
+(put 's1 'a 5)
+(get 's1 'b)
