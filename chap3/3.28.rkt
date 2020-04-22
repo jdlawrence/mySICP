55caa21f0d62090eaed82c873b5e2929b39ef923
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-04-21 21:27:29 -0700

    Simplify logical-or in 3.28

--- chap3/3.28.rkt
+++ chap3/3.28.rkt
@@ -1,17 +1,16 @@
 #lang sicp
 
 (define (logical-or o1 o2)
-  (cond
-    ((and (= o1 0) (= o2 0)) 0)
-    ((and (= o1 0) (= o2 1)) 1)
-    ((and (= o1 1) (= o2 0)) 1)
-    ((and (= o1 1) (= o2 1)) 1)
-        (else (error "Invalid signal" o1 o2))))
+  (if (or (= o1 1) (= o2 1))
+      1
+      0))
 
 (logical-or 0 0)
 (logical-or 0 1)
 (logical-or 1 0)
 (logical-or 1 1)
+
+#|
 (define (or-gate o1 o2 output)
   (define (or-action-procedure)
     (let ((new-value
@@ -22,4 +21,5 @@
   (add-action! o1 or-action-procedure)
   (add-action! o2 or-action-procedure)
   'ok)
+|#
 
