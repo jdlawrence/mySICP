0f09577cbc395cb3d017ba752d900f4ca24d3a64
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-04-22 21:17:19 -0700

    3.29 add alternative solution

--- chap3/3.29.rkt
+++ chap3/3.29.rkt
@@ -24,3 +24,14 @@
   (add-action! o2 or-action-procedure)
   'ok)
 
+#| More concise version below that uses prior definitions |#
+
+(define (or-gate a1 a2 output) 
+   (let ((c1 (make-wire)) 
+         (c2 (make-wire)) 
+         (c3 (make-wire))) 
+     (inverter a1 c1) 
+     (inverter a2 c2) 
+     (and-gate c1 c2 c3) 
+     (inverter c3 output))) 
+
