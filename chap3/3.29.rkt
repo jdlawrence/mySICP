e481dde2b36bc916411ac565ae445f8f6d2ea52a
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-04-22 21:19:17 -0700

    3.29 typo fix

--- chap3/3.29.rkt
+++ chap3/3.29.rkt
@@ -27,11 +27,11 @@
 #| More concise version below that uses prior definitions |#
 
 (define (or-gate a1 a2 output) 
-   (let ((c1 (make-wire)) 
-         (c2 (make-wire)) 
-         (c3 (make-wire))) 
-     (inverter a1 c1) 
-     (inverter a2 c2) 
-     (and-gate c1 c2 c3) 
-     (inverter c3 output))) 
+  (let ((c1 (make-wire)) 
+        (c2 (make-wire)) 
+        (c3 (make-wire))) 
+    (inverter a1 c1) 
+    (inverter a2 c2) 
+    (and-gate c1 c2 c3) 
+    (inverter c3 output))) 
 
