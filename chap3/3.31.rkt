b656cb7673eca0ba30b91fc9d750ef543ffbd8b2
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-04-25 12:29:41 -0700

    3.31 complete

--- chap3/3.31.rkt
+++ chap3/3.31.rkt
@@ -1,62 +1,12 @@
 #lang sicp
-
-(define (logical-not s)
-  (cond ((= s 0) 1)
-        ((= s 1) 0)
-        (else (error "Invalid signal" s))))
-
-(define (logical-and s1 s2)
-  (if (and (= s1 1) (= s2 1)) 1)
-  1
-  0)
-
-(define (logical-or s1 s2)
-  (if (or (= s1 1) (= s2 1)) 1)
-  1
-  0)
-
-(define (make-wire)
-  (let ((signal-value 0) (action-procedures '()))
-    (define (set-my-signal! new-value)
-      (if (not (= signal-value new-value))
-          (begin (set! signal-value new-value)
-                 (call-each action-procedures))
-          'done))
-    (define (accept-action-procedure! proc)
-      (set! action-procedures
-            (cons proc action-procedures))
-      (proc))
-    (define (dispatch m)
-      (cond ((eq? m 'get-signal) signal-value)
-            ((eq? m 'set-signal!) set-my-signal!)
-            ((eq? m 'add-action!) accept-action-procedure!)
-            (else (error "Unknown operation: WIRE" m))))
-    dispatch))
-
-(define (get-signal wire) (wire 'get-signal))
-(define (set-signal! wire new-value)
-((wire 'set-signal!) new-value))
-(define (add-action! wire action-procedure)
-((wire 'add-action!) action-procedure))
-
-(define (and-gate a1 a2 output)
-  (define (and-action-procedure)
-    (let ((new-value
-           (logical-and (get-signal a1) (get-signal a2))))
-      (after-delay
-       and-gate-delay
-       (lambda () (set-signal! output new-value)))))
-  (add-action! a1 and-action-procedure)
-  (add-action! a2 and-action-procedure)
-  'ok)
-
-(define (half-adder a b s c)
-  (let ((d (make-wire)) (e (make-wire)))
-    (or-gate a b d)
-    (and-gate a b c)
-    (inverter c e)
-    (and-gate d e s)
-    'ok))
+#|
+The add-action procedure is called in called in the definition of 'inverter',
+'and-gate', and 'or-gate', and it where the delay, logic, and watching of
+input changes for for each function takes place. If we didn't call the procedure
+right away, the changes in signal would never be called, and the time delays
+wouldn't take place. We wouldn't get any change in output for the signals and the
+time delay wouldn't be correct at all.
+|#
 
 
             
