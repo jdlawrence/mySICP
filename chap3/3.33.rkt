6edbedc1c1f8465e41f56033470366023488853a
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-04-30 08:44:23 -0700

    3.33 Add more concise solution

--- chap3/3.33.rkt
+++ chap3/3.33.rkt
@@ -102,6 +102,47 @@
   (connect sum me)
   me)
 
+(define (multiplier m1 m2 product)
+  (define (process-new-value)
+    (cond ((or (and (has-value? m1) (= (get-value m1) 0))
+               (and (has-value? m2) (= (get-value m2) 0)))
+           (set-value! product 0 me))
+          ((and (has-value? m1) (has-value? m2))
+           (set-value! product
+                       (* (get-value m1) (get-value m2))
+                       me))
+          ((and (has-value? product) (has-value? m1))
+           (set-value! m2
+                       (/ (get-value product)
+                          (get-value m1))
+                       me))
+          ((and (has-value? product) (has-value? m2))
+           (set-value! m1
+                       (/ (get-value product)
+                          (get-value m2))
+                       me))))
+  (define (process-forget-value)
+    (forget-value! product me)
+    (forget-value! m1 me)
+    (forget-value! m2 me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value) (process-new-value))
+          ((eq? request 'I-lost-my-value) (process-forget-value))
+          (else (error "Unknown request: MULTIPLIER"
+                       request))))
+  (connect m1 me)
+  (connect m2 me)
+  (connect product me)
+  me)
+
+(define (constant value connector)
+  (define (me request)
+    (error "Unknown request: CONSTANT" request))
+  (connect connector me)
+  (set-value! connector value me)
+  me)
+
 #| Actual Answer: |#
 
 (define (averager a b c)
@@ -132,7 +173,17 @@
   (connect c me)
   me)
 
+#| Much more concise solution |#
 
+(define (averager-concise a b c)
+  (let (
+        (u (make-connector))
+        (v (make-connector))
+        )
+    (adder a b u)
+    (constant 2 v)
+    (multiplier c v u)
+    'ok))
 
 #| Testing |#
 (define a1 (make-connector))
@@ -149,7 +200,8 @@
 (define b (make-connector))
 (define c (make-connector))
 
-(averager a b c)
+;(averager a b c)
+(averager-concise a b c)
 
 (probe "a" a)
 (probe "b" b)
