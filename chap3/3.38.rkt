68244077c2b260b021f0ef2eb62d364f6c409270
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-05-04 09:14:00 -0700

    3.38 WIP

---
+++ chap3/3.38.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+#| Part a |#
+
+#|
+Peter: (set! balance (+ balance 10))
+Paul: (set! balance (- balance 20))
+Mary: (set! balance (- balance (/ balance 2)))
+|#
+
+
+#|
+Let's note order like this: (Peter, Paul, Mary) means Peter then Paul then Mary
+
+Starting with $100:
+
+(Peter, Paul, Mary) = $45
+(Peter, Mary, Paul) = $35
+(Paul, Peter, Mary) = $45
+(Paul, Mary, Peter) = $50
+(Mary, Peter, Paul) = $40
+(Mary, Paul, Peter) = $40
+|#
