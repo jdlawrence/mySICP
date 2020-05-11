3ab89e212f59d4800119f7d2171d409beb5b3fbb
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-05-10 19:31:50 -0700

    3.42 complete

---
+++ chap3/3.42.rkt
@@ -0,0 +1,29 @@
+#lang sicp
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw (protected withdraw))
+          (protected-deposit (protected deposit)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) protected-withdraw)
+              ((eq? m 'deposit) protected-deposit)
+              ((eq? m 'balance) balance)
+              (else
+               (error "Unknown request: MAKE-ACCOUNT"
+                      m))))
+      dispatch)))
+
+#| Answer
+This change makes no difference at all. We're still returning the protected versions
+of withdraw and deposit.
+|#
+
+
