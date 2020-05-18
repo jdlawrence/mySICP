5b1b3a1fa3b950e7437a801a83ab06f282ba7462
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-05-18 08:54:55 -0700

    3.47 continues

--- chap3/3.47.rkt
+++ chap3/3.47.rkt
@@ -53,23 +53,66 @@ s
                  (the-mutex 'acquire))) ; retry
             ((eq? m 'release) (clear! cell))))
     the-mutex))
+
 (define (clear! cell) (set-car! cell false))
 
+#| Answer in terms of mutexes |#
+
 (define (make-semaphore n )
-  (let ((count 0))
+  (let (
+        (count 0) ; To count the number of mutexes currently active
+        (lock (make-mutex)) ; To make sure our actual don't interleave with each other
+        )
     (define (dispatch m)
       (cond ((eq? m 'acquire)
+             (lock 'acquire) ; Use the lock to 'acquire so that we won't be interrupted
+             ; If count is is less than n, call 'release on the lock mutex and increase the count
              (if (< count n)
                  (begin
-                   ((make-mutex) 'acquire)
-                   (set! count (+ count 1)))
-                 (dispatch 'acquire)))
+                   (set! count (+ count 1))
+                   (lock 'release)) ; Release when we we're done 
+                 ; If not, call 'release, but keep trying to acquire
+                 (begin
+                   (lock 'release)
+                   (dispatch 'acquire))))
+            ; If release is called, assuming count is greater than 0 and we have mutexes to release,
+            ; decrease the count with an 'acquire and 'release 
             ((eq? m 'release)
              (if (> count 0)
+                 (lock 'acquire)
                  (begin
                    (set! count (- count 1))
-                   ; Maybe do something else to release a mutex?
-                   )))))
+                   (lock 'release))))))
     dispatch))
 
 
+#| Answer in terms of test-and-set! |#
+(define (make-semaphore n )
+  (let (
+        (count 0) ; To count the number of mutexes currently active
+        (lock (list false)) ; Use a single cell
+        )
+    (define (dispatch m)
+      (cond ((eq? m 'acquire)
+             ; If we're busy, then keep retrying by dispatching 'acquire
+             (if (test-and-set! lock)
+                 (dispatch 'acquire))
+             ; If count is is less than n, call test-and-set! and increase count
+             (test-and-set! lock)
+             (if (< count n)
+                 (begin
+                   (set! count (+ count 1))
+                   (clear! lock)) ; Clear when we we're done 
+                 ; If not, call 'release, but keep trying to acquire
+                 (begin
+                   (clear! lock)
+                   (dispatch 'acquire))))
+             ; If release is called, assuming count is greater than 0 and we have mutexes to release,
+             ; decrease the count with an 'acquire and 'release 
+            ((eq? m 'release)
+             (if (> count 0)
+                 (lock 'acquire)
+                 (begin
+                   (set! count (- count 1))
+                   (lock 'release))))))
+    dispatch))
