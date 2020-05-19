acc797a09a3c1500f7977bff75e3bd74d590e5ec
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-05-19 09:20:45 -0700

    3.48 WIP

---
+++ chap3/3.48.rkt
@@ -0,0 +1,7 @@
+#lang sicp
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
