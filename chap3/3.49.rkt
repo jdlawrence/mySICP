81ea35037878f8353d64203873206b6aec61be55
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-05-22 11:09:05 -0700

    3.49 complete

---
+++ chap3/3.49.rkt
@@ -0,0 +1,6 @@
+#lang sicp
+#| Answer
+If we don't know in advance which which account we're working with, we cannot get the ids
+of each one, and therefore cannot determine the order in which serialization should
+proceed. We need to know which accounts are going to be exchanged ahead of time.
+|#
