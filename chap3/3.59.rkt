fe0ddb0546a0e356154ef7dee6699650744e31d6
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-06-23 19:11:10 -0700

    3.50 complete

---
+++ chap3/3.59.rkt
@@ -0,0 +1,94 @@
+#lang sicp
+
+(define (display-line x) (newline) (display x))
+(define (stream-car stream)
+  (car stream))
+
+(define the-empty-stream '())
+
+(define (stream-cdr stream)
+  (force (cdr stream)))
+
+(define (stream-null? stream)
+  (null? stream))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter
+                       pred
+                       (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define ones (cons-stream 1 ones))
+
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+(define integers
+  (cons-stream 1 (add-streams ones integers)))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor))
+              stream))
+
+#| Answer:|#
+
+#| Part A: |#
+(define (integrate-series input)
+  (define (sub term-num s)
+    (if (stream-null? s)
+      the-empty-stream
+      (cons-stream
+       (* (/ 1 term-num) (stream-car s))
+       (sub (+ term-num 1) (stream-cdr s)))))
+  (sub 1 input))
+
+#| Part B: |#
+(define cosine-series (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+(define sine-series (cons-stream 0 (integrate-series cosine-series)))
+
+#| Testing |#
+;(define ex (integrate-series integers))
+(define ex (integrate-series ones))
+
+(stream-ref ex 0)
+(stream-ref ex 1)
+(stream-ref ex 2)
+(stream-ref ex 3)
