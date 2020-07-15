d01e05076edbc09307462cf0a7c769afc9bdfa26
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-07-14 19:39:56 -0700

    3.65 WIP

---
+++ chap3/3.65.rkt
@@ -0,0 +1,148 @@
+#lang sicp
+
+(define (display-line x) (newline) (display x))
+(define (stream-car stream)
+  (car stream))
+
+(define the-empty-stream '())
+
+(define (stream-cdr stream)
+  (force (cdr stream)))
+
+(define (stream-null? stream)
+  (null? stream))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (show x)
+  (display-line x)
+  x)
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter
+                       pred
+                       (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define ones (cons-stream 1 ones))
+
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+(define integers
+  (cons-stream 1 (add-streams ones integers)))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor))
+              stream))
+
+
+(define (integrate-series input)
+  (define (sub term-num s)
+    (if (stream-null? s)
+        the-empty-stream
+        (cons-stream
+         (* (/ 1 term-num) (stream-car s))
+         (sub (+ term-num 1) (stream-cdr s)))))
+  (sub 1 input))
+
+(define cosine-series (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+(define sine-series (cons-stream 0 (integrate-series cosine-series)))
+
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) (stream-car s2))
+               (add-streams (scale-stream (stream-cdr s1) (stream-car s2))
+                            (mul-series s1 (stream-cdr s2)))))
+
+(define (invert-unit-series s)
+  (cons-stream 1 (scale-stream (mul-series (stream-cdr s) (invert-unit-series s)) -1)))
+
+(define (div-series s1 s2)
+  (if (= (stream-car s2) 0)
+      (error "cannot divide by zero")
+      (mul-series s1 (invert-unit-series s2))))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (partial-sums s) 
+  (add-streams s (cons-stream 0 (partial-sums s))))
+
+#| Square root iterative function |#
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream
+     1.0
+     (stream-map (lambda (guess) (sqrt-improve guess x))
+                 guesses)))
+  guesses)
+
+(define (display-n-terms s n)
+  (define (sub count)
+    (if (= count n)
+        (stream-ref s count)
+        (begin
+          (stream-ref s count)
+          (sub (+ count 1)))))
+  (sub 0))
+#| Answer |#
+#| This summands stream converges VERY slowly |#
+(define (ln-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (ln-summands (+ n 1)))))
+
+(define ln-stream
+  (partial-sums (ln-summands 1)))
+
+
+
+#| Testing |#
+(display-n-terms ln-stream 10)
+#|
+(stream-ref ln-stream 0)
+(stream-ref ln-stream 1)
+(stream-ref ln-stream 2)
+(stream-ref ln-stream 3)
+(stream-ref ln-stream 4)
+(stream-ref ln-stream 5)
+(stream-ref ln-stream 6)
+(stream-ref ln-stream 7)
+(stream-ref ln-stream 8)
+(stream-ref ln-stream 9)
+(stream-ref ln-stream 10)
+|#
