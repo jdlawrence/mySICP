d594bddff250ab1094310a0a78fd62b84c44a474
Jamil Lawrence <jamil.lawrence@gmail.com>
2020-12-30 16:07:43 -0800

    4.11 in progress

---
+++ chap4/4.11.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(define ns (make-base-namespace))
+
+#| Answer:
+Currently, we represent a frame as a pair of lists, like so:
+
+(define (make-frame variables values)
+  (cons variables values))
+
+The variables are the "car" of the list and their values are the "cdr" of it.
+
+|#
+
+;We could change to something like this:
+
+(list pair1 pair2 pair3)
+
+;To get the name of one pair, we need the car of it, and to get the value we
+;take the cdr
+
+;To get all of the frame variables, we can map "car" to a frame
+(define (frame-variables frame) (map car frame))
+
+;To get all of the frame values, we can map "cdr" to a frame
+(define (frame-variables frame) (map cdr frame))
+
+;To add a frame, previously we just cons the variable and value on to their
+;respective lists, but now we can the pair onto the list
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons (list var val) (car frame))))
