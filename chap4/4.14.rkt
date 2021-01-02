f4dacd169a6de9ee66662fa553dfaf08ebc936da
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-02 11:16:50 -0800

    4.14 WIP

---
+++ chap4/4.14.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+#| Answer:
+Louis tries to install the system definition of map in the metacircular evaluator,
+but this is being used by primitive-procedure-names and primitive-procedure-objects.
+Therefore a causes a ton of problems.
+
+
+|#
+
+
+
+
+
