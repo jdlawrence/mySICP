80ac280b3677006f355a96030d6733c30a7bb138
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-02 20:11:25 -0800

    4.14 complete

--- chap4/4.14.rkt
+++ chap4/4.14.rkt
@@ -4,7 +4,7 @@ Louis tries to install the system definition of map in the metacircular evaluato
 but this is being used by primitive-procedure-names and primitive-procedure-objects.
 Therefore a causes a ton of problems.
 
-
+I'm not really too clear on this exercise, but hopefully it will become more clear in the future.
 |#
 
 
