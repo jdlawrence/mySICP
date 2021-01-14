4427a3c51e719ee1e347acfdfe825a45739c08ba
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-13 16:41:29 -0800

    4.31 skipped for now

---
+++ chap4/4.31.rkt
@@ -0,0 +1,7 @@
+#lang sicp
+#| I can see where the authors are going with this exercise, but it seems extensive, and
+I'd rather learn other concepts that are more relevant to my interests.
+
+Skipping for now.
+
+|#
