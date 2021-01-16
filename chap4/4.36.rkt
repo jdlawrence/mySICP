ae3013cee1575f7ee01127ed123ebf575d786d58
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-15 16:48:41 -0800

    4.36 complete

--- chap4/4.36.rkt
+++ chap4/4.36.rkt
@@ -12,21 +12,22 @@ the i and j outer lists will never increment.
         (list i j k)))))
 
 #| Answer:
-To accomplish this, I think we could use a procedure where typing "try-again" increases the upper
+We need to increment the numbers in a manner where the i and j increase (as well as the k). To accomplish this,
+I think we could use a procedure where typing "try-again" increases the upper
 boundary of a call to a function I'll define called an-integer-up-to.
 |#
 
-(define (an-integer-up-to start high)
-  (require (<= start high))
-  (amb start (an-integer-up-to (+ start 1) high)))
+(define (an-integer-up-to current high)
+  (require (< current high))
+  (amb current (an-integer-up-to (+ current 1) high)))
 
 
 #| Every time "try-again" is pressed, we increase the value of "high" passed to all-pythagorean-triples
 |#
 (define (all-pythagorean-triples high)
-  (let ((i (an-integer-up-to high)))
-    (let ((j (an-integer-up-to high)))
-      (let ((k (an-integer-up-to high)))
+  (let ((i (an-integer-starting-from 1)))
+    (let ((j (an-integer-up-to 1 i)))
+      (let ((k (an-integer-up-to 1 j)))
         (require (= (+ (* i i) (* j j)) (* k k)))
         (list i j k)))))
            
