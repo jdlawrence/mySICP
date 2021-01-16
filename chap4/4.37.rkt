c625283cc1a351324fdb821ce07395e3875529db
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-15 16:55:04 -0800

    4.37 good attempt

---
+++ chap4/4.37.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+#| Answer:
+Ben's method is not more efficient. Checking if all of the "k"s are integers is very inefficent.
+|#
+
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high))
+        (hsq (* high high)))
+    (let ((j (an-integer-between i high)))
+      (let ((ksq (+ (* i i) (* j j))))
+        (require (>= hsq ksq))
+        (let ((k (sqrt ksq)))
+          (require (integer? k))
+          (list i j k))))))😂
