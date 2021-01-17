8dc7a9b138308399ab375fafb22ddc777e67066b
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-17 10:39:20 -0800

    4.39 redone.

--- chap4/4.39.rkt
+++ chap4/4.39.rkt
@@ -1,5 +1,16 @@
 #lang sicp
 #| Answer:
-I do think the order matters, but I believe this is the fastest order. I'd need to test it to be sure,
-but the code to do so is not available until the next section. I will return to this one.
+I believe that the order does not matter for the total number of outcomes, but that it will matter
+for the total time to completion. Ideally you'd pick something that eliminates the most possibilities
+at once. For example, putting the statement:
+
+(require (not (= (abs (- smith fletcher)) 1)))
+
+before others doesn't directly eliminate any choices for fletcher, so you must go through all the choices
+for him before going back and eliminating the appropriate ones for smith. However if you put these statements:
+
+(require (not (= fletcher 5)))
+(require (not (= fletcher 1)))
+
+first, then you have less an easier choice for smith.
 |#
