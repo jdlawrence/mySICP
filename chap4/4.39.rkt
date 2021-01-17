7f95c165e0d67d49f1ba4201cd8219c31c4e7967
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-17 10:27:26 -0800

    4.39 complete for now

---
+++ chap4/4.39.rkt
@@ -0,0 +1,5 @@
+#lang sicp
+#| Answer:
+I do think the order matters, but I believe this is the fastest order. I'd need to test it to be sure,
+but the code to do so is not available until the next section. I will return to this one.
+|#
