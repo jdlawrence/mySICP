f37723235bb601916632bbe3c5920a81e77ec93d
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-29 15:48:48 -0800

    4.47 addendum

--- chap4/4.47.rkt
+++ chap4/4.47.rkt
@@ -12,6 +12,10 @@
 Louis Reasoner's implementation below assumes that there MUST be a prepositional phrase
 in the verb phrase. If there isn't one, his implementation will break. The given one (above)
 does not assume that and surrounds the part with the prepositional phrase in a "maybe-extend".
+
+We cannot change the order of the expressions in the "amb" portion. If we do and we put
+(parse-verb-phrase) first, amb will choose it first, and get stuck in an infinite loop because
+it keeps calling itself.
 |#
 (define (parse-verb-phrase)
   (amb (parse-word verbs)
