34cc4dd936a29e7e8113f669237ca6e35cf75a7e
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-01-29 16:05:47 -0800

    4.49 WIP

---
+++ chap4/4.49.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+(define adjectives '(adjective hungry turquoised poignant ridiculous))
+
+#|  We need to alter parse-word |#
+
+(define (parse-word word-list)
+  (require (not (null? *unparsed*)))
+  (require (memq (car *unparsed*) (cdr word-list)))
+  (let ((found-word (car *unparsed*)))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
