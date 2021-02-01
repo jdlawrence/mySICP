91f073c1bca2f333073fb9812a9ab62a3fc2f126
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-02-01 14:45:34 -0800

    4.51 redone.

--- chap4/4.51.rkt
+++ chap4/4.51.rkt
@@ -1,9 +1,28 @@
 #lang sicp
 
+; Old definition
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail)         ; *1*
+               (let ((old-value
+                      (lookup-variable-value var env))) 
+                 (set-variable-value! var val env)
+                 (succeed 'ok
+                          (lambda ()    ; *2*
+                            (set-variable-value! var
+                                                 old-value
+                                                 env)
+                            (fail)))))
+             fail))))
 #| Answer:
 
 This involves not-calling the reassignment near the *2* to set the var back to it's old-value.
-Instead I just passed a dummy lambda that just returns the empty list
+Instead we just call set with vproc and never reset the value.
+
+If we just the normal version of this function, we'll get (a b 1), (a c 1), (b a 1), etc
 
 |#
 (define (analyze-assignment exp)
@@ -12,14 +31,6 @@ Instead I just passed a dummy lambda that just returns the empty list
     (lambda (env succeed fail)
       (vproc env
              (lambda (val fail2)        ; *1*
-               (let ((old-value
-                      (lookup-variable-value var env))) 
-                 (set-variable-value! var val env)
-                 (succeed 'ok
-                          (lambda ()    ; *2*
-                            ; (set-variable-value! var
-                            ;                      old-value
-                            ;                      env)
-                            ((lambda () '()))
-                            (fail2)))))
+               (set-variable-value! var val env) 
+               (succeed 'ok  fail2)) 
              fail))))
--- chap4/metacircular-eval-4-3.rkt
+++ chap4/metacircular-eval-4-3.rkt
@@ -549,17 +549,16 @@
         (vproc (analyze (assignment-value exp))))
     (lambda (env succeed fail)
       (vproc env
-             (lambda (val fail2)        ; *1*
+             (lambda (val fail)         ; *1*
                (let ((old-value
                       (lookup-variable-value var env))) 
                  (set-variable-value! var val env)
                  (succeed 'ok
                           (lambda ()    ; *2*
-                           ; (set-variable-value! var
-                           ;                      old-value
-                           ;                      env)
-                            ((lambda () '()))
-                            (fail2)))))
+                            (set-variable-value! var
+                                                 old-value
+                                                 env)
+                            (fail)))))
              fail))))
 
 ;;;Procedure applications
