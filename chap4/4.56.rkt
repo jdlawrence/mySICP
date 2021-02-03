b01b587ebbf92f48b4e078cfcce7c7eb6c5f2371
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-02-03 15:29:32 -0800

    4.56 revised

--- chap4/4.56.rkt
+++ chap4/4.56.rkt
@@ -3,7 +3,7 @@
 
 a. the names of all people who are supervised by Ben
 Bitdiddle, together with their addresses;|#
-(and (supervisor ?person (Ben Bitdiddle))
+(and (supervisor ?person (Bitdiddle Ben))
      (address ?person ?where))
 #|
 b. all people whose salary is less than Ben Bitdiddle’s,
@@ -14,18 +14,18 @@ together with their salary and Ben Bitdiddle’s salary;
 (salary (Ben Bitdiddle) ?amount)
 
 ; Then this query
-(or
- (and (salary ?person ?amount) (lisp-value > ?amount 60000))
- (salary (Ben Bitdiddle) ?amount))
+(and (salary (Bitdiddle Ben) ?benSalary)
+     (salary ?person ?personSalary)
+     (lisp-value < ?benSalary ?personSalary))
 
 #|
 c. all people who are supervised by someone who is not
 in the computer division, together with the supervisor’s name and job.
 |#
 (and
- (not (supervisor ?person (computer . ?other)))
- (supervisor ?person ?person2))
+ (supervisor ?supervisorName ?person)
+ (not (job ?person (computer . ?other)))
+ (job ?supervisorName ?supervisorJob)) 
 
 
 
- 
