b50c20541cadadc6263b38e8fcc4414823418e8e
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-02-03 10:53:42 -0800

    4.54 addendum

--- chap4/4.54.rkt
+++ chap4/4.54.rkt
@@ -1,4 +1,22 @@
 #lang sicp
+#|
+(define (analyze-require exp)
+  (let ((pproc (analyze (require-predicate exp))))
+    (lambda (env succeed fail)
+      (pproc env
+             (lambda (pred-value fail2)
+               (if ⟨??⟩
+                   ⟨??⟩
+                   (succeed 'ok fail2)))
+             fail))))
+|#
+
+#| This given code is a bit strange because the success code, (succeed 'ok fail2)
+is in the alternative portion of the if. Therefore we need to write the logic
+such that if the predicate is NOT true, we use the consequence portion of the "if"
+statement. If the pred-value is false, then we use the first "fail", but if it's
+true, we will used the passed in fail2.
+|#
 (if (not (true? pred-value)) 
-    (fail2) 
+    (fail) 
     (succeed 'ok fail2)) 
