6ff7fe3ad1564f8396a453c0f85cb675771ef8a1
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-02-15 14:29:25 -0800

    4.67 complete

---
+++ chap4/4.67.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+#| Answer:
+The basic operation would be to store a list of all the queries, and if a query has been conducted with
+certain arguments already, then we assume there's a loop.
+
+I'll leave the details of this alone for now.
+
+|#
